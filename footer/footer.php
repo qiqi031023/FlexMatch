0ba6508fdd67ebe9894d7c7c132fd9edf83c1c12
qiqi031023 <chuaqi@graduate.utm.my>
2024-11-07 07:01:44 +0000

    Update footer.php,image,jobseeker.php and report_form.php

---
+++ footer/footer.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        footer {
+            background-color: #333;
+            color: #fff;
+            padding: 1rem;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <?php
+        echo '<footer>
+            <p>&copy; 2024 Copyright: <b>FlexMatch</b>. All rights reserved.</p>
+        </footer>'
+    ?>
+</body>
+</html>
